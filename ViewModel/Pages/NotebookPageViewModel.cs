36679904d9bfda0f4a6ee22a786b22265a37dc7a
xjkxksj <106696465+xjkxksj@users.noreply.github.com>
2024-05-14 11:17:32 +0200

    More changes in XAML
    Dialog window for adding notes,

--- ViewModel/Pages/NotebookPageViewModel.cs
+++ ViewModel/Pages/NotebookPageViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Notatnik_WPF;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Notatnik_WPF;
 internal class NotebookPageViewModel
 {
+    public ICommand AddNoteCommand { get; set; }
 
+    public NotebookPageViewModel()
+    {
+        AddNoteCommand = new RelayCommand(AddNote);
+    
+    }
+    public void AddNote()
+    {
+        AddNoteDialogWindow addNoteDialogWindow = new AddNoteDialogWindow();
+        addNoteDialogWindow.ShowDialog();
+    }
 }
